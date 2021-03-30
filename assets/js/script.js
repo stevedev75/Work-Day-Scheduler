212c510e7b9045cd59bb319a6d503c39ea5b016a
Steve Dever <stephendever.cr@gmail.com>
2021-03-30 11:54:40 -0400

    add basic planner grid and clickable 'save' buttons

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+var today = moment();
+$("#currentDay").text(today.format("MMM Do, YYYY"));
+
+var now = moment().format('LT'); 
+console.log(now);
+
+var time;
+
+
+// if (time = "now") {
+// add class .present to div class description};
+// }
+// else if (time = "past") {
+// add class .past to div class description);
+// }
+// else {
+// add class.future to div class description);
+// };
+
+
+// how to get "description" to accept input??
+
+// how to get "saveBtn" to save to local storage??
